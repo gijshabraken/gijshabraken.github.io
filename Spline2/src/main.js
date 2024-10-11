164a78a7bd8fffe96929a79c5b1b8d49bd7e091b
Gijs Habraken <info@gijshabraken.nl>
2024-10-11 17:04:16 +0200

    Nieuwe Spline test

--- Spline2/src/main.js
+++ Spline2/src/main.js
@@ -2,4 +2,4 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/36DBMP5oLDbp7WD7/scene.splinecode');
+app.load('scene.splinecode');
