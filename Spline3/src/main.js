8dffc22e8efdd2d36fd94d3f27700d2b7afc81c5
Gijs Habraken <info@gijshabraken.nl>
2024-11-04 10:38:17 +0100

    Nieuwe export Peacock Kube met externe URL

---
+++ Spline3/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/FJcb8zCiz59D238v/scene.splinecode');
