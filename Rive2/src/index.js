005e5024fadca51fe6e607edd6d2f2375065c68c
Gijs Habraken <info@gijshabraken.nl>
2024-11-19 15:23:47 +0100

    Logo test

---
+++ Rive2/src/index.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+
+const rive = require("@rive-app/canvas");
+
+let stateMachineLoadInput;
+const r = new rive.Rive({
+  src:
+    "peacock-logo-v1i.riv",
+  canvas: document.getElementById("rivecanvas"),
+  stateMachines: "ScrollingBehaviour",
+  artboard: "horizontal",
+  autoplay: true,
+  layout: new rive.Layout({
+    fit: rive.Fit.FitWidth,
+    alignment: rive.Alignment.CenterLeft
+  }),
+  onLoad: () => {
+    stateMachineLoadInput = r.stateMachineInputs("ScrollingBehaviour")[0];
+    stateMachineLoadInput.value = 1;
+  }
+});
+
+const body = document.querySelector("body");
+const bodyParent = body.parentNode;
+const pageLength = bodyParent.scrollHeight;
+
+// const pctLabel = document.getElementById("pctLabel");
+document.onscroll = () => {
+  const scrollOffset = bodyParent.scrollTop;
+  const scrollPctValue =
+    (100 * scrollOffset) / (pageLength - bodyParent.clientHeight);
+  // pctLabel.innerHTML = `Scroll Percentage: ${scrollPctValue.toFixed(0)}%`;
+  stateMachineLoadInput.value = scrollPctValue;
+};
