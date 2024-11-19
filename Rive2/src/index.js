3db6dca412bd070e02fa37845a3de4b3f71773ff
Gijs Habraken <info@gijshabraken.nl>
2024-11-19 16:15:36 +0100

    nieuwe logoscroll test

--- Rive2/src/index.js
+++ Rive2/src/index.js
@@ -1,4 +1,6 @@
-const rive = require("https://unpkg.com/@rive-app/canvas@2.7.0");
+
+
+const rive = import * as rive from "https://unpkg.com/@rive-app/canvas@2.7.0";
 
 let stateMachineLoadInput;
 const r = new rive.Rive({
